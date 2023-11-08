f3f00717e42025d71724ea3e4baca0ee43192c65
RMompati <mmpterold@gmail.com>
2023-11-08 09:58:10 +0200

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
-import { useReducer } from "react";
+import { useRef } from "react";
 
 function App() {
 
-  const [checked, setChecked] = useReducer((checked) => !checked, false, undefined);
+  const txtTitle = useRef();
+  const hexColor = useRef();
 
+  console.log(txtTitle)
+  console.log(hexColor)
+  const submit = (e) => {
+    e.preventDefault();
+
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+
+    alert(`${title}, ${color}`);
+
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
   return (
     <div className="App">
-      <label>
-        <input type="checkbox"
-               value={checked}
-               onChange={setChecked}/>
-        {checked ? "Checked" : "Not Checked"}
-      </label>
+      <form onSubmit={submit}>
+        <input ref={txtTitle} className="App-Input" type="text" placeholder="color title..."/>
+        <input ref={hexColor} type="color"/>
+        <br/>
+        <button className="App-Button">Add</button>
+      </form>
     </div>
   );
 }
