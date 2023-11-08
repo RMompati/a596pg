479db291d50c9043b8b39c701b6b96abee269966
RMompati <mmpterold@gmail.com>
2023-11-08 14:46:08 +0200

    fetching data from a GraphQL API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-function useInput(initialValue = "") {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {value, onChange: (e) => setValue(e.target.value)},
-    () => setValue(initialValue)
-  ];
-}
+const query = `
+query {
+  allLifts {
+    name,
+    elevationGain,
+    status
+  }
+}`;
+ const opts = {
+   method: "POST",
+   headers: { "Content-Type": "application/json"},
+   body: JSON.stringify({ query })
+ };
 
-function GitHubUser ({ name, location, avatar_url}) {
+
+function Lift ({ name, elevationGain, status}) {
   return (
       <div className="App">
         <h2>{name}</h2>
         <hr style={{width: "25%"}}/>
-        <p>Based in: {location}</p>
-        <img src={avatar_url} height={200} alt={"Github Avatar"}/>
+        <p>{elevationGain} {status}</p>
       </div>
   );
 }
@@ -23,13 +29,13 @@ function GitHubUser ({ name, location, avatar_url}) {
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    fetch(`https://api.github.com/users/RMompati`)
+    fetch(`https://snowtooth.moonhighway.com/`, opts)
         .then((response) => response.json())
-        .then(setData)
+        .then((data) => setData(data.data))
         .then(() => setLoading(false))
         .catch(setError);
   }, []);
@@ -38,8 +44,10 @@ function App() {
   if (error) return <pre>{JSON.stringify(error)}</pre>
   return (
     <div>
-      <h1 className="App">Github API Data</h1>
-      { data ? <GitHubUser {...data}/> : <h2>No data...</h2> }
+      <h1 className="App">GraphQL API Data</h1>
+      {
+        data?.allLifts.map((lift) => <Lift {...lift}/>)
+      }
     </div>
   );
 }
