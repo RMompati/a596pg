bccd6f8687326532fa8d724350ef3723795f7863
RMompati <mmpterold@gmail.com>
2023-11-13 09:06:45 +0200

    Rendering the right data at the right time. Render props...

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import './App.css';
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name,
-    elevationGain,
-    status
-  }
-}`;
- const opts = {
-   method: "POST",
-   headers: { "Content-Type": "application/json"},
-   body: JSON.stringify({ query })
- };
+const peaks = [
+  { name: "Freel", elevation: 10891},
+  { name: "Monument", elevation: 10067},
+  { name: "Pyramid", elevation: 9983},
+  { name: "Tallac", elevation: 9735},
+];
 
-
-function Lift ({ name, elevationGain, status}) {
-  return (
-      <div className="App">
-        <h2>{name}</h2>
-        <hr style={{width: "25%"}}/>
-        <p>{elevationGain} {status}</p>
-      </div>
-  );
+function List({data, renderItem, renderEmpty}) {
+  return !data.length ? renderEmpty
+      :(
+          <ul>
+            {
+              data.map((item) => (
+                  <li key={item.name}>{renderItem(item)}</li>
+              ))
+            }
+          </ul>
+      );
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://snowtooth.moonhighway.com/`, opts)
-        .then((response) => response.json())
-        .then((data) => setData(data.data))
-        .then(() => setLoading(false))
-        .catch(setError);
-  }, []);
-
-  if (loading) return <h1 className="App">Loading...</h1>
-  if (error) return <pre>{JSON.stringify(error)}</pre>
+  /** This <> ... </> is a shorthand for <React.Fragment /> */
   return (
-    <div>
-      <h1 className="App">GraphQL API Data</h1>
-      {
-        data?.allLifts.map((lift) => <Lift {...lift}/>)
-      }
-    </div>
+      <List data={peaks}
+            renderEmpty={<p>This list is empty</p>}
+            renderItem={item => (
+                <>
+                  {item.name} - {item.elevation} ft.
+                </>
+            )}
+      />
   );
 }
 
